d59564afd9122ed3dc33cad5223e2c4f8eeba649
Ford Marklew <fmarklew@my.centennialcollege.ca>
2018-05-08 17:42:15 -0400

    Implemented movement, jumping, and started camera work

---
+++ XionAdventurePlatformer/Assets/Scripts/Player_GroundDetection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_GroundDetection : MonoBehaviour {
+
+    public bool isGrounded;
+    public float r;
+
+    public LayerMask cf2D;
+
+    public float offsetY;
+
+    private void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(new Vector2(transform.position.x, (transform.position.y-offsetY)),r,cf2D);
+    }
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(new Vector2(transform.position.x, transform.position.y-offsetY), r);
+    }
+}
---
+++ XionAdventurePlatformer/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour {
+    [Header ("Speeds")]
+    public float moveSpeed;
+    public float jumpSpeed;
+
+    private float moveDir;
+    private Rigidbody2D rb;
+
+    private Player_GroundDetection PGD;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        PGD = GetComponent<Player_GroundDetection>();
+    }
+
+    void Update () {
+        moveDir = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveSpeed * moveDir, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && PGD.isGrounded){
+            Jump();
+        }
+
+	}
+
+    void Jump()
+    {
+        PGD.isGrounded = false;
+        rb.AddForce(new Vector2(rb.velocity.x, jumpSpeed), ForceMode2D.Impulse);
+    }
+}
