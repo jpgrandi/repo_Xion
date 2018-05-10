d80478dc539709394240b339e345a8f3663dcb83
João Pedro Lomba Grandinetti <jp@grandi.net>
2018-05-10 14:27:28 -0400

    GitHub repo is now up to date with Bitbucket repo. Joe, 2018_05_10

---
+++ XionAdventurePlatformer/Assets/Scripts/Enemy_AI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_AI : MonoBehaviour {
+
+    public float moveSpeed;
+
+    private Rigidbody2D rb;
+
+    private GameObject detectionBox;
+
+    void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        detectionBox = gameObject.transform.GetChild(0).gameObject;
+
+    }
+
+    private void Update()
+    {
+        rb.velocity = new Vector2(moveSpeed, 0);
+        if (!detectionBox.GetComponent<Enemy_AI_Detection>().isTouchingGround)
+        {
+            moveSpeed = -moveSpeed;
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        }
+    }
+}
---
+++ XionAdventurePlatformer/Assets/Scripts/Enemy_AI_Detection.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_AI_Detection : MonoBehaviour {
+
+    public bool isTouchingGround;
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isTouchingGround = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isTouchingGround = true;
+    }
+}
